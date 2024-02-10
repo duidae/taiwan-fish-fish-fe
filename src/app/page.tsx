cb310158f6716f11a2d27e300bcb4817b2422ff7
duidae <duidae@gmail.com>
2024-02-10 22:44:03 +0800

    fix: refine a little bit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,26 +7,34 @@ export default function Home() {
   const ytURL = 'https://www.youtube.com/embed/aod40An1DLQ?si=_Y18xwVEIoOHQTLJ'
   const posts = postOGs
 
+  const interactiveSection = (
+    <div className='flex flex-col w-full justify-center items-center min-h-screen'>
+      <div className="flex flex-row w-full">
+        <Video ytURL={ytURL}/>
+        <Playground/>
+      </div>
+      <div>看文章</div>
+    </div>
+  )
+
+  const postSection = (
+    <div className="mb-32 grid text-center lg:max-w-6xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      {posts.map((post, index) =>
+        <PostCard
+          key={`post-list-${index}`}
+          url={post.url}
+          ogImage={post.ogImage}
+          ogTitle={post.ogTitle}
+          ogDescription={post.ogDescription}
+        />
+      )}
+    </div>
+  )
+
   return (
     <main className="flex flex-col w-full items-center justify-between">
-      <div className='flex flex-col w-full justify-center items-center min-h-screen'>
-        <div className="flex flex-row w-full">
-          <Video ytURL={ytURL}/>
-          <Playground/>
-        </div>
-        <div>看文章</div>
-      </div>
-      <div className="mb-32 grid text-center lg:max-w-6xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        {posts.map((post, index) =>
-          <PostCard
-            key={`post-list-${index}`}
-            url={post.url}
-            ogImage={post.ogImage}
-            ogTitle={post.ogTitle}
-            ogDescription={post.ogDescription}
-          />
-        )}
-      </div>
+      {interactiveSection}
+      {postSection}
     </main>
   )
 }
