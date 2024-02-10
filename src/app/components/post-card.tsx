191a5eb15f0a51807e2de87151ef3e3a07ff16fe
duidae <duidae@gmail.com>
2024-02-10 18:27:22 +0800

    fix: fix coding style

--- src/app/components/post-card.tsx
+++ src/app/components/post-card.tsx
@@ -2,24 +2,25 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 export const PostCard = (props: { url: string, ogImage: string, ogTitle: string, ogDescription: string}) => {
-    return (props.url && 
-        <Link
-        href={props.url}
-        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        target="_blank"
-        rel="noopener noreferrer"
-        >
-          <Image src={props.ogImage}
-            width={500}
-            height={500}
-            alt="Picture of the author"
-          />
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            {props.ogTitle}{" "}
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            {props.ogDescription}
-          </p>
-        </Link>
-    )
-  }
+  return (props.url &&
+    <Link
+      href={props.url}
+      className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Image
+        src={props.ogImage}
+        width={500}
+        height={500}
+        alt="Picture of the author"
+      />
+      <h2 className={`mb-3 text-2xl font-semibold`}>
+        {props.ogTitle}{" "}
+      </h2>
+      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+        {props.ogDescription}
+      </p>
+    </Link>
+  )
+}
