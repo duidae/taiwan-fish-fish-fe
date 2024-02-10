c07e8c188776b01bf25bb02b4ce938142bd711ab
duidae <duidae@gmail.com>
2024-02-10 16:36:35 +0800

    fix: fix map name

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
         <Link href="/">魚傳媒</Link>
         <Link href="/topic">專題</Link>
         <Link href="/post">文章</Link>
-        <Link href="/map">找找魚</Link>
+        <Link href="/map">魚地圖</Link>
         {SOCIAL_MEDIA.map((media, index) => <a key={`social-media-link-${index}`} href={media.url} target='_blank'>{media.icon}</a>)}
       </div>
     )
