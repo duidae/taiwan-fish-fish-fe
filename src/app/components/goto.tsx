bf3d34f73d432af03bd28c822cd262ae12ff043c
duidae <duidae@gmail.com>
2024-02-11 17:28:51 +0800

    fix: fix home

--- src/app/components/goto.tsx
+++ src/app/components/goto.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import { HEADER_HEIGHT } from '@/app/constant'
+
 export const Goto = (props: { elementID: string, className?: string, children?: React.ReactNode }) => {
   return (
     <button className={props.className} onClick={() => {
       const element = document.querySelector(`#${props.elementID}`) as HTMLElement
       window.scrollTo({
-        top: element.offsetTop,
+        top: element.offsetTop - HEADER_HEIGHT,
         behavior: 'smooth',
       })
     }}>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
+import Link from 'next/link'
 import { Video } from '@/app/components/video'
 import { Playground } from '@/app/components/playground'
 import { PostCard } from '@/app/components/post-card'
 import { Goto } from '@/app/components/goto'
 import { postOGs } from './post-mockups'
 
-const postSectionID = 'post-section'
+const contentSectionID = 'content-section'
 
 export default function Home() {
   const ytURL = 'https://www.youtube.com/embed/aod40An1DLQ?si=_Y18xwVEIoOHQTLJ'
-  const posts = postOGs
+
+  const contents = [{
+    title: '專題',
+    route: '/topics',
+    items: postOGs
+  },
+  {
+    title: '文章',
+    route: '/posts',
+    items: postOGs
+  }]
 
   const interactiveSection = (
     <div className='bg-blue-200 flex flex-col w-full h-screen justify-center items-center pt-24'>
@@ -16,28 +27,40 @@ export default function Home() {
         <Video className='w-1/3' ytURL={ytURL}/>
         <Playground className='w-2/3'/>
       </div>
-      <Goto className='h-1/6' elementID={postSectionID}>看文章</Goto>
+      <Goto className='h-1/6 text-lg' elementID={contentSectionID}>看文章</Goto>
     </div>
   )
 
-  const postSection = (
-    <div id={postSectionID} className="mt-16 mb-16 grid text-center w-4/5 lg:grid-cols-4 lg:text-left">
-      {posts.map((post, index) =>
-        <PostCard
-          key={`post-list-${index}`}
-          url={post.url}
-          ogImage={post.ogImage}
-          ogTitle={post.ogTitle}
-          ogDescription={post.ogDescription}
-        />
-      )}
+  const contentSection = (
+    <div id={contentSectionID}>
+      {contents.map((content, index) => {
+        return (
+          <div key={`content-${index}`} className='flex flex-col justify-center items-center pt-8 mb-4'>
+            <div className='text-2xl'>
+              最新{content.title}
+            </div>
+            <div className="mt-8 mb-4 grid text-center w-4/5 lg:grid-cols-4 lg:text-left">
+              {content.items.map((item, index) =>
+                <PostCard
+                  key={`post-list-${index}`}
+                  url={item.url}
+                  ogImage={item.ogImage}
+                  ogTitle={item.ogTitle}
+                  ogDescription={item.ogDescription}
+                />
+              )}
+            </div>
+            <Link className='text-base' href={content.route}>看全部{content.title}{' >>'}</Link>
+          </div>
+        )
+      })}
     </div>
   )
 
   return (
-    <main className="flex flex-col w-full items-center justify-between">
+    <main className="flex flex-col w-full items-center justify-between mb-8">
       {interactiveSection}
-      {postSection}
+      {contentSection}
     </main>
   )
 }
