7f89732bc3b7d6098e6a53690a5d5665f97de90e
duidae <duidae@gmail.com>
2024-02-11 19:51:44 +0800

    feat: add routes

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -5,8 +5,8 @@ export const Header = () => {
   return (
     <nav className="fixed top-0 w-4/5 max-w-6xl bg-transparent p-4 z-10 items-center justify-between font-mono text-lg text-white lg:flex">
       <Link href="/">魚傳媒</Link>
-      <Link href="/topic">專題</Link>
-      <Link href="/post">文章</Link>
+      <Link href="/topics">專題</Link>
+      <Link href="/posts">文章</Link>
       <Link href="/map">魚地圖</Link>
       <div className='flex flex-row gap-1'>
         {SOCIAL_MEDIA.map((media, index) => <a key={`social-media-link-${index}`} href={media.url} target='_blank'>{media.icon}</a>)}
