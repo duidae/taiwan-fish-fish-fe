ee59984fac3dde46a47baa71082bb4d393532cf4
duidae <duidae@gmail.com>
2024-02-11 18:26:04 +0800

    fix: adjust layout

--- src/app/components/video.tsx
+++ src/app/components/video.tsx
@@ -1,7 +1,7 @@
 export const Video = (props: { ytURL: string, className?: string }) => {
   // Autoplay only works when autoplay=1&mute=1
   return (
-    <div className={`${props.className} flex flex-col justify-center ml-10 mr-5`}>
+    <div className={`${props.className} flex flex-col justify-center ml-20 mr-5`}>
       <iframe width="100%" src={`${props.ytURL}&autoplay=1&mute=1`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
     </div>
   )
