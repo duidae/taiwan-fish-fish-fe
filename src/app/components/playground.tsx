39044bf26601ff47b21511866c712dc8039024fc
duidae <duidae@gmail.com>
2024-02-11 15:37:13 +0800

    feat: integrate p5

--- src/app/components/playground.tsx
+++ src/app/components/playground.tsx
@@ -1,7 +1,27 @@
+'use client'
+
+import { type Sketch } from "@p5-wrapper/react"
+import { NextReactP5Wrapper } from "@p5-wrapper/next"
+
+const sketch: Sketch = (p5) => {
+  p5.setup = () => p5.createCanvas(600, 400, p5.WEBGL)
+
+  p5.draw = () => {
+    p5.background(250)
+    p5.normalMaterial()
+    p5.push();
+    p5.rotateZ(p5.frameCount * 0.01)
+    p5.rotateX(p5.frameCount * 0.01)
+    p5.rotateY(p5.frameCount * 0.01)
+    p5.plane(100)
+    p5.pop()
+  }
+}
+
 export const Playground = (props: { className?: string }) => {
   return (
     <div className={`${props.className} border-black border-2 flex flex-col justify-center ml-5 mr-10`}>
-      {'P5'}
+      <NextReactP5Wrapper sketch={sketch} />
     </div>
   )
 }
