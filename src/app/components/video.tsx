a3a9ff71c84a9c3def7456c205f2811f2b0f2867
duidae <duidae@gmail.com>
2024-02-13 18:07:12 +0800

    feat: integrate video slider

--- src/app/components/video.tsx
+++ src/app/components/video.tsx
@@ -1,8 +1,30 @@
-export const Video = (props: { ytURL: string, className?: string }) => {
-  // Autoplay only works when autoplay=1&mute=1
+'use client'
+import { useRef } from 'react'
+import { register } from 'swiper/element/bundle'
+
+// Register swiper web components(<swiper-container>, <swiper-slide>)
+register()
+
+export const Video = (props: { ytURLs: string[], className?: string }) => {
+  const swiperElRef = useRef(null)
+
+  // Autoplay only works when &autoplay=1&mute=1
   return (
     <div className={`${props.className} flex flex-col justify-center ml-20 mr-5`}>
-      <iframe width="100%" src={`${props.ytURL}&autoplay=1&mute=1`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      <swiper-container
+        ref={swiperElRef}
+        pagination="true"
+        pagination-clickable="true"
+        style={{ width: '100%', height: '100%' }}
+      >
+        {props.ytURLs.map((url, index) => {
+          return (url &&
+            <swiper-slide key={`yt-slide-${index}`}>
+              <iframe width="100%" src={url} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen/>
+            </swiper-slide>
+          )
+        })}
+      </swiper-container>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,11 @@ import { postOGs } from './post-mockups'
 const contentSectionID = 'content-section'
 
 export default function Home() {
-  const ytURL = 'https://www.youtube.com/embed/aod40An1DLQ?si=_Y18xwVEIoOHQTLJ'
+  const ytURLs = [
+    'https://www.youtube.com/embed/aod40An1DLQ?si=_Y18xwVEIoOHQTLJ',
+    'https://www.youtube.com/embed/wPQnp65MYz4?si=s9NnnMzG6nXJzh_j',
+    'https://www.youtube.com/embed/NehqK_atfVQ?si=-F0PfGHz09MyvJwl',
+  ]
 
   const contents = [{
     title: '專題',
@@ -28,7 +32,7 @@ export default function Home() {
       imgSrcs={[]}
       body={
         <div className="flex flex-row w-full h-full">
-          <Video className='w-1/3' ytURL={ytURL}/>
+          <Video className='w-1/3' ytURLs={ytURLs}/>
           <Playground className='w-2/3'/>
         </div>
       }
