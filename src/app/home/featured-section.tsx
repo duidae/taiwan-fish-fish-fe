77a32191fb23656a35e18c966de9edc72d3af7c1
duidae <duidae@gmail.com>
2024-03-07 18:04:58 +0800

    fix: fix home

--- src/app/home/featured-section.tsx
+++ src/app/home/featured-section.tsx
@@ -5,15 +5,17 @@ export enum DIRECTION {
   Right = "RIGHT"
 }
 
-export const FeaturedSection = (props: {id: string; title: string; route: string; direction?: DIRECTION}) => {
-  const {id, title, route, direction} = props
+type FeaturedSectionProps =  {id: string; title: string; route: string; frontPage: React.ReactNode, featured: React.ReactNode, direction?: DIRECTION}
+
+const FeaturedSection = (props:FeaturedSectionProps) => {
+  const {id, title, route, frontPage, featured, direction} = props
   const isLeft = direction === DIRECTION.Left
 
   return (
     <div className="w-full min-h-screen flex flex-col justify-center items-center">
       <div id={id} className="w-full grow flex flex-row justify-center items-stretch">
-        <div className={`${isLeft ? "w-3/5" : "w-2/5"} bg-red-50`}></div>
-        <div className={`${isLeft ? "w-2/5" : "w-3/5"} bg-green-50`}></div>
+        <div className={`${isLeft ? "w-3/5" : "w-2/5"} bg-red-50`}>{frontPage}</div>
+        <div className={`${isLeft ? "w-2/5" : "w-3/5"} bg-green-50`}>{featured}</div>
       </div>
       <Link className="h-1/6 text-base" href={route}>
         看全部{title}
@@ -21,4 +23,25 @@ export const FeaturedSection = (props: {id: string; title: string; route: string
       </Link>
     </div>
   )
+}
+
+export const FeaturedVideos = (props:FeaturedSectionProps) => {
+  const {frontPage, featured, ...rest} = props
+
+  // TODO: customize frontPage/featured
+  return <FeaturedSection frontPage={featured} featured={featured} {...rest}/>
+}
+
+export const FeaturedPosts = (props:FeaturedSectionProps) => {
+  const {frontPage, featured, ...rest} = props
+
+  // TODO: customize frontPage/featured
+  return <FeaturedSection frontPage={featured} featured={featured} {...rest}/>
+}
+
+export const FeaturedTopics = (props:FeaturedSectionProps) => {
+  const {frontPage, featured, ...rest} = props
+
+  // TODO: customize frontPage/featured
+  return <FeaturedSection frontPage={featured} featured={featured} {...rest}/>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,34 +2,13 @@ import {FullscreenBackground} from "@/app/components/fullscreen-background"
 import {Video} from "@/app/components/video"
 import {Playground} from "@/app/components/playground"
 import {Goto} from "@/app/components/goto"
-import {FeaturedSection, DIRECTION} from "@/app/home/featured-section"
-import {chillYTVideos, featuredPosts} from "./mockups"
+import {FeaturedVideos, FeaturedPosts, FeaturedTopics, DIRECTION} from "@/app/home/featured-section"
+import {chillYTVideos, featuredImages} from "./mockups"
 
 export default function Home() {
-  const featuredSections = [
-    {
-      id: "featured-section-video",
-      title: "影片",
-      route: "/videos",
-      summaries: featuredPosts
-    },
-    {
-      id: "featured-section-post",
-      title: "文章",
-      route: "/posts",
-      summaries: featuredPosts
-    },
-    {
-      id: "featured-section-topic",
-      title: "專題",
-      route: "/topics",
-      summaries: featuredPosts
-    }
-  ]
-
   const interactiveSection = (
     <FullscreenBackground
-      imgSrcs={[/*"/test1.webp", "/test2.webp", "/test3.jpeg",*/ "/ai1.jpeg", "/ai2.jpeg"]}
+      imgSrcs={featuredImages}
       body={
         <div className="flex flex-row w-full h-full">
           <Video className="w-1/3" ytURLs={chillYTVideos} />
@@ -37,28 +16,40 @@ export default function Home() {
         </div>
       }
       bottom={
-        <Goto className="h-1/6 text-xl text-white" elementID={featuredSections[0].id}>
+        <Goto className="h-1/6 text-xl text-white" elementID={"featured-videos"}>
           看內容
         </Goto>
       }
     />
   )
 
-  const featuredSectionsJSX = featuredSections.map((featuredSection, index) => {
-    return (
-      <FeaturedSection
-        id={featuredSection.id}
-        title={featuredSection.title}
-        route={featuredSection.route}
-        direction={index % 2 == 0 ? DIRECTION.Left : DIRECTION.Right}
-      />
-    )
-  })
-
   return (
     <main className="flex flex-col w-full items-center justify-between mb-8">
       {interactiveSection}
-      {featuredSectionsJSX}
+      <FeaturedVideos
+          id="featured-videos"
+          title="影片"
+          route="/videos"
+          direction={DIRECTION.Left}
+          frontPage={<div></div>}
+          featured={<div></div>}
+          />
+      <FeaturedPosts
+          id="featured-posts"
+          title="文章"
+          route="/posts"
+          direction={DIRECTION.Right}
+          frontPage={<div></div>}
+          featured={<div></div>}
+          />
+      <FeaturedTopics
+          id="featured-topics"
+          title="專題"
+          route="/topics"
+          direction={DIRECTION.Left}
+          frontPage={<div></div>}
+          featured={<div></div>}
+          />
     </main>
   )
 }
