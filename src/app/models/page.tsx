8118411d238cbc8957279fe06a7b95a92a49cb41
duidae <duidae@gmail.com>
2024-03-08 21:15:14 +0800

    fix: fix listing page

--- src/app/models/page.tsx
+++ src/app/models/page.tsx
@@ -4,11 +4,19 @@ export default function Models() {
   const featuredModels = _models
   const models = _models
 
+  const slider = <div>Slider</div>
+
+  const modelCompnents = models.map((model, index) => (
+    <div key={`embed-model-${index}`} dangerouslySetInnerHTML={{__html: model}} />
+  ))
+
   return (
     <main className="flex flex-col w-full items-center justify-between mt-16 mb-8">
-      {models.map((model, index) => (
-        <div key={`embed-model-${index}`} dangerouslySetInnerHTML={{__html: model}} />
-      ))}
-    </main>
+    <div className="text-2xl">所有模型</div>
+    {slider}
+    <div className="mt-8 mb-4 grid text-center w-4/5 lg:grid-cols-3 lg:text-left gap-8">
+      {modelCompnents}
+    </div>
+  </main>
   )
 }
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,12 +1,30 @@
-import {List} from "@/app/components/list"
-import {featuredPosts} from "../mockups"
+import {SummaryCard, DisplayMode} from "@/app/components/summary-card"
+import {featuredPosts as _featuredPosts} from "../mockups"
 
 export default function Posts() {
-  const postSummaries = [...featuredPosts, ...featuredPosts]
+  const featuredPosts = _featuredPosts
+  const posts = [..._featuredPosts, ..._featuredPosts]
+
+  const slider = null
+
+  const postComponents = posts.map((summary, index) => (
+    <SummaryCard
+      key={`post-list-${index}`}
+      url={summary.url}
+      ogImage={summary.ogImage}
+      ogTitle={summary.ogTitle}
+      ogDescription={summary.ogDescription}
+      displayMode={DisplayMode.COLUMN}
+    />
+  ))
 
   return (
     <main className="flex flex-col w-full items-center justify-between mt-16 mb-8">
-      <List summaries={postSummaries} title="所有文章" />
+      <div className="text-2xl">所有文章</div>
+      {slider}
+      <div className="mt-8 mb-4 grid text-center w-4/5 lg:grid-cols-4 lg:text-left gap-8">
+        {postComponents}
+      </div>
     </main>
   )
 }
