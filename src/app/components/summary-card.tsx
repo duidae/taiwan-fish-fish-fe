42265b4672c2f5a0b8e141d045b435a5ee241ed7
duidae <duidae@gmail.com>
2024-03-08 15:08:28 +0800

    fix: fix layout

--- src/app/components/summary-card.tsx
+++ src/app/components/summary-card.tsx
@@ -12,12 +12,14 @@ export type Summary = {
 
 export const SummaryCard = (props: Summary) => {
   const {url, ogTitle, ogDescription, ogImage, direction} = props
+  const isLeft = direction === DIRECTION.Left
 
+  // TODO: ellipsis for <p>
   return (
     url && (
       <Link
         href={url}
-        className={`w-full h-full flex ${direction === DIRECTION.Left ? "flex-row" : "flex-row-reverse"} gap-2 items-center justify-stretch px-4 group rounded-lg border border-transparent transition-colors hover:bg-gray-100`}
+        className={`w-full h-full flex ${isLeft ? "flex-row" : "flex-row-reverse"} gap-2 items-center justify-stretch px-4 group rounded-lg border border-transparent transition-colors hover:bg-gray-100`}
         target="_blank"
         rel="noopener noreferrer"
       >
@@ -26,9 +28,7 @@ export const SummaryCard = (props: Summary) => {
           <img className="overflow-hidden" src={ogImage} alt={ogTitle} />
         </div>
         <div className="w-2/3">
-          <h2 className={`mb-3 text-2xl ${direction === DIRECTION.Left ? "text-right" : "text-left"} font-semibold`}>
-            {ogTitle}{" "}
-          </h2>
+          <h2 className={`mb-3 text-2xl ${isLeft ? "text-right" : "text-left"} font-semibold`}>{ogTitle}</h2>
           <p className="w-full m-0 text-sm opacity-50">{ogDescription}</p>
         </div>
       </Link>
