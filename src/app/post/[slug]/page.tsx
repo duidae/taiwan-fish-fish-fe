a8d061c757677fb4657e19399e54997af4aaa4c2
duidae <duidae@gmail.com>
2024-03-14 23:39:33 +0800

    fix: fix post page

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,9 +1,44 @@
+import {Metadata} from "next"
+import {notFound} from "next/navigation"
 import {PostRenderer} from "./post-renderer"
 
 // TODO: remove mockup
 import {postMockup} from "@/app/mockups"
 
-export default function Post() {
+export async function generateMetadata({params}: {params: {slug: string}}): Promise<Metadata> {
+  const {slug} = params
+
+  return {
+    /*
+    title: `${topicMeta?.ogTitle ? topicMeta.ogTitle + ' - ' : ''}${OG_SUFFIX}`,
+    alternates: {
+      canonical: `${KIDS_URL_ORIGIN}/topic/${slug}`,
+    },
+    openGraph: {
+      title: topicMeta?.ogTitle ?? OG_SUFFIX,
+      description: topicMeta?.ogDescription ?? GENERAL_DESCRIPTION,
+      images: topicMeta?.ogImage?.resized?.small
+        ? [topicMeta.ogImage.resized.small]
+        : [],
+    },
+    other: {
+      // Since we can't inject <!-- <PageMap>...</PageMap> --> to <head> section with Next metadata API,
+      // so handle google seo with extra <meta> tag here, but be awared there are limitations(maximum 50 tags):
+      // https://developers.google.com/custom-search/docs/structured_data?hl=zh-tw#limitations
+      publishedDate: topicMeta?.publishedDate ?? '',
+      contentType: ContentType.TOPIC,
+    },
+    */
+  }
+}
+
+export default async function Post({params}: {params: {slug: string}}) {
+  const {slug} = params
+
+  if (!slug) {
+    return notFound()
+  }
+
   const content = postMockup
 
   return (
