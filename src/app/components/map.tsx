04d171ae0b4bde80dd0b00212d30ad045e48cd49
duidae <duidae@gmail.com>
2024-03-22 16:23:07 +0800

    fix: refine a little bit

--- src/app/components/map.tsx
+++ src/app/components/map.tsx
@@ -10,6 +10,13 @@ import MarkerShadow from "../../node_modules/leaflet/dist/images/marker-shadow.p
 */
 import "leaflet/dist/leaflet.css"
 
+// iNaturalist API doc: https://api.inaturalist.org/v1/docs/
+
+// place_id=131031 - taiwan
+// iconic_taxa=Actinopterygii - fish
+const searchURL = "https://api.inaturalist.org/v1/observations?place_id=131031&iconic_taxa=Actinopterygii"
+const taxanomyURLPrefix = "https://www.inaturalist.org/taxa"
+
 const DEFAULT_ZOOM = 8
 const TAIPEI_CENTER = [25.038357847174, 121.54770626982]
 const TAIWAN_CENTER = [23.973837, 120.97969]
@@ -21,10 +28,6 @@ const TAIWAN_CENTER = [23.973837, 120.97969]
   url="https://api.gbif.org/v2/map/occurrence/density/{z}/{x}/{y}@1x.png?style=classic.point&srs=EPSG%3A3857&taxonKey=1"
 />
 */
-
-// TODO: query fish - https://www.inaturalist.org/observations?place_id=131031&subview=map&view=species&iconic_taxa=Actinopterygii
-// place_id=131031 - taiwan
-// iconic_taxa=Actinopterygii - fish
 
 const Item = (props: {checked: boolean; onChange: () => void; children?: React.ReactNode}) => {
   const {checked, onChange, children} = props
@@ -44,9 +47,7 @@ const Map = () => {
   useEffect(() => {
     // TODO: remove duplications, add infinite load
     const fetchObs = async () => {
-      const obs = await axios.get(
-        "https://api.inaturalist.org/v1/observations?place_id=131031&iconic_taxa=Actinopterygii"
-      )
+      const obs = await axios.get(searchURL)
       if (obs?.data?.results) {
         setTaxons(obs.data.results)
       } else {
@@ -79,14 +80,21 @@ const Map = () => {
 
   const taxonItems = (
     <>
-      {taxons.map(taxon => (
-        <Item onChange={() => handleSelect(taxon.taxon.id)} checked={taxonIDs.includes(taxon.taxon.id)}>
-          <a href={`https://www.inaturalist.org/taxa/${taxon.taxon.id}`} target="_blank">
-            <img src={taxon.taxon.default_photo.medium_url} />
-            <span>{taxon.species_guess}</span>
-          </a>
-        </Item>
-      ))}
+      {taxons?.map(result => {
+        const taxonID = result.taxon.id
+        const taxaURL = `${taxanomyURLPrefix}/${result.taxon.id}`
+        const imgURL = result.taxon.default_photo.medium_url
+        const title = result.species_guess
+
+        return (
+          <Item onChange={() => handleSelect(taxonID)} checked={taxonIDs.includes(taxonID)}>
+            <a href={taxaURL} target="_blank">
+              <img src={imgURL} />
+              <span>{title}</span>
+            </a>
+          </Item>
+        )
+      })}
     </>
   )
 
