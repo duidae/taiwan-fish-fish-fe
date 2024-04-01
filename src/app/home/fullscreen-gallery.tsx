119067538feb475ceb5e5facb86e2b017bdeec36
duidae <duidae@gmail.com>
2024-04-01 11:26:08 +0800

    fix: refine a little bit

--- src/app/home/fullscreen-gallery.tsx
+++ src/app/home/fullscreen-gallery.tsx
@@ -1,36 +1,7 @@
 "use client"
 import {useState} from "react"
-import styled from "styled-components"
 import {GetRandomInteger} from "@/app/utils"
 import {arrowLeft, arrowRight} from "@/app/icons"
-
-const bulletColor = "rgb(113, 113, 113)"
-const bulletHeight = 60
-
-const Bullets = styled.ul`
-  margin: 0;
-  padding-left: 0;
-  padding-top: 20px;
-  list-style-type: none;
-  text-align: center;
-  width: 100%;
-  height: ${bulletHeight}px;
-`
-
-const Bullet = styled.li<{$isActive: boolean}>`
-  width: 0.6em;
-  height: 0.6em;
-  min-width: 10px;
-  min-height: 10px;
-  margin: 0 0.8em;
-  border-radius: 50%;
-  display: inline-block;
-  background-color: ${props => (props.$isActive ? "white" : bulletColor)};
-  transition: background-color 0.6s ease;
-  overflow: hidden;
-  text-indent: 100%;
-  cursor: pointer;
-`
 
 export type Gallery = {
   url: string
@@ -53,6 +24,19 @@ export const FullscreenGallery = (props: {gallerySrcs: Gallery[]; body: React.Re
     onImageChange(currentIndex + 1)
   }
 
+  const galleryController = (
+    <div className="flex flex-row">
+      <button onClick={onPrevImage}>{arrowLeft}</button>
+      <button onClick={onNextImage}>{arrowRight}</button>
+    </div>
+  )
+
+  const description = (
+    <div className="absolute w-1/4 right-0 bottom-0 rounded-md p-4 m-4 bg-slate-50 bg-opacity-50">
+      {gallerySrcs[currentIndex].desc}
+    </div>
+  )
+
   return (
     <div
       style={{
@@ -62,18 +46,8 @@ export const FullscreenGallery = (props: {gallerySrcs: Gallery[]; body: React.Re
       className="bg-black bg-no-repeat bg-center bg-contain flex flex-col w-full h-screen justify-center items-center pt-24"
     >
       {body}
-      <div className="flex flex-row">
-        <button onClick={onPrevImage}>{arrowLeft}</button>
-        <button onClick={onNextImage}>{arrowRight}</button>
-      </div>
-      <Bullets>
-        {gallerySrcs?.map((gallerySrc, index) => (
-          <Bullet key={`img-bullet-${index}`} $isActive={index === currentIndex} onClick={() => onImageChange(index)} />
-        ))}
-      </Bullets>
-      <div className="absolute w-1/4 right-0 bottom-0 rounded-md p-4 m-4 bg-slate-50 bg-opacity-50">
-        {gallerySrcs[currentIndex].desc}
-      </div>
+      {galleryController}
+      {description}
     </div>
   )
 }
