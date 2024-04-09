525e0f5ee47524f3490aff6bd938d69505ed5d3b
duidae <duidae@gmail.com>
2024-04-09 22:00:30 +0800

    fix: refine slider style

--- src/app/components/slider.tsx
+++ src/app/components/slider.tsx
@@ -10,15 +10,20 @@ export const Slider = (props: {slides: React.ReactNode[]}) => {
   return (
     <div className="w-full h-full flex">
       <swiper-container
+        style={{
+          width: "100%",
+          "--swiper-pagination-color": 'var(--theme-blue)',
+          "--swiper-pagination-bullet-inactive-color": 'gray',
+          "--swiper-pagination-bullet-inactive-opacity": '1',
+          "--swiper-navigation-color": 'white',
+        }}
         autoplay-delay="3500"
         slides-per-view="3"
         navigation="true"
         pagination="true"
         pagination-clickable="true"
-        pagination-dynamic-bullets="true"
         loop="true"
         space-between="10"
-        style={{width: "100%"}}
       >
         {slides?.map((slide, index) => {
           return slide && <swiper-slide key={`slide-${index}`}>{slide}</swiper-slide>
