9ab85240e977e53350059718b5a3bfdc3d501dd8
duidae <duidae@gmail.com>
2024-04-18 00:10:36 +0800

    fix: simplify header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,23 +1,29 @@
 import Link from "next/link"
-import {SITE_NAME, ROUTE_HEADER, Z_INDEX} from "@/app/constant"
-import {SubscribeBtn} from "./subscription"
+import {ROUTE_HEADER, Z_INDEX} from "@/app/constant"
 
 export const Header = () => {
+  const logo = (
+    <Link className='mt-4 ml-16' href="/">
+      <img className="h-20" src="/logo-horizontal.svg" />
+    </Link>
+  )
+
+  const routes = ROUTE_HEADER.map((route, index) => (
+    <Link key={`header-router-${index}`} href={route.path}>
+      {route.title}
+    </Link>
+  ))
+
   return (
     <div
       style={{backgroundImage: "linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(0,0,0,0))"}}
-      className="fixed top-0 w-full flex flex-row justify-center"
+      className="fixed top-0 w-full flex flex-row justify-between"
     >
+      {logo}
       <nav
-        className={`w-full max-w-6xl p-4 items-center justify-between font-mono text-lg text-white lg:flex ${Z_INDEX.TOP}`}
+        className={`w-full max-w-4xl flex flex-row justify-end items-start font-mono text-lg text-white mt-6 mr-4 gap-16 ${Z_INDEX.TOP}`}
       >
-        <Link href="/">{SITE_NAME}</Link>
-        {ROUTE_HEADER.map((route, index) => (
-          <Link key={`header-router-${index}`} href={route.path}>
-            {route.title}
-          </Link>
-        ))}
-        <SubscribeBtn />
+        {routes}
       </nav>
     </div>
   )
