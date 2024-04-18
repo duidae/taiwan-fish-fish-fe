4cafca2f13a2633931965ac64024d81a45f3116d
duidae <duidae@gmail.com>
2024-04-18 15:08:25 +0800

    fix: fix rwd

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,13 +3,13 @@ import {ROUTE_HEADER, Z_INDEX} from "@/app/constant"
 
 export const Header = () => {
   const logo = (
-    <Link className="mt-4 ml-16" href="/">
+    <Link className="mt-4 ml-10" href="/">
       <img className="h-20" src="/logo-horizontal.svg" />
     </Link>
   )
 
   const routes = ROUTE_HEADER.map((route, index) => (
-    <Link key={`header-router-${index}`} href={route.path}>
+    <Link className='hover:underline decoration-2 underline-offset-8' key={`header-router-${index}`} href={route.path}>
       {route.title}
     </Link>
   ))
@@ -21,7 +21,7 @@ export const Header = () => {
     >
       {logo}
       <nav
-        className={`w-full max-w-4xl flex flex-row justify-end items-start font-mono text-lg text-white mt-6 mr-4 gap-16 ${Z_INDEX.TOP}`}
+        className={`w-full max-w-4xl flex flex-row justify-end items-start font-mono text-md md:text-lg text-white mt-4 mr-8 pt-2 gap-4 md:gap-8 lg:gap-16 ${Z_INDEX.TOP}`}
       >
         {routes}
       </nav>
