a0c4053136ec8bcc2da7780d0400069a7c726ef5
許守傑 Shou-Chieh Hsu (Jason) <duidae@gmail.com>
2024-04-28 11:04:09 +0800

    Jason/add component animation (#31)
    * fix: fix style
    
    * fix: fix header
    
    * fix: refine a little bit

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,7 +1,20 @@
+"use client"
+import {useState} from "react"
 import Link from "next/link"
 import {ROUTE_HEADER, Z_INDEX} from "@/app/constant"
+import {CrossIcon, HamburgerIcon} from "@/app/assets/icons"
 
 export const Header = () => {
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false)
+
+  const onHamburgerOpen = () => {
+    setIsHamburgerOpen(true)
+  }
+
+  const onHamburgerClose = () => {
+    setIsHamburgerOpen(false)
+  }
+
   const logo = (
     <Link className="mt-4 ml-10" href="/">
       <img className="h-20" src="/logo-horizontal.svg" />
@@ -15,15 +28,40 @@ export const Header = () => {
     </Link>
   ))
 
+  const menu = (
+    <nav className="hidden w-full max-w-4xl md:flex flex-row justify-end items-start font-mono text-md md:text-lg text-white mt-4 mr-8 pt-2 gap-4 md:gap-8 lg:gap-16">
+      {routes}
+    </nav>
+  )
+
+  const mobileMenu = (
+    <>
+      {!isHamburgerOpen && (
+        <div className="block md:hidden mt-4 mr-8 cursor-pointer" onClick={onHamburgerOpen}>
+          {HamburgerIcon}
+        </div>
+      )}
+      {isHamburgerOpen && (
+        <div className="absolute right-0 top-0 md:hidden flex flex-col p-4 bg-slate-700">
+          <div className="flex flex-row justify-end cursor-pointer" onClick={onHamburgerClose}>
+            {CrossIcon}
+          </div>
+          <nav className="max-w-4xl flex flex-col justify-end items-start font-mono text-md md:text-lg text-white pt-2 gap-4 md:gap-8 lg:gap-16">
+            {routes}
+          </nav>
+        </div>
+      )}
+    </>
+  )
+
   return (
     <div
       style={{backgroundImage: "linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(0,0,0,0))"}}
       className={`fixed top-0 w-full flex flex-row justify-between ${Z_INDEX.TOP}`}
     >
       {logo}
-      <nav className="w-full max-w-4xl flex flex-row justify-end items-start font-mono text-md md:text-lg text-white mt-4 mr-8 pt-2 gap-4 md:gap-8 lg:gap-16">
-        {routes}
-      </nav>
+      {menu}
+      {mobileMenu}
     </div>
   )
 }
