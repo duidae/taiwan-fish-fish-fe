ca3081979ff8bbdb0655796cdf3715cca1d89379
duidae <duidae@gmail.com>
2024-04-30 21:44:06 +0800

    fix: fix footer rwd

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -9,7 +9,7 @@ export const Footer = () => {
 
   const about = (
     <div className="flex flex-col gap-2">
-      <h2 className="mb-4">{"關於我們"}</h2>
+      <h2 className="text-xl md:text-2xl mb-4">{"關於我們"}</h2>
       <Link href={ROUTE_ABOUT.path}>{ROUTE_ABOUT.title}</Link>
       <SubscribeBtn />
     </div>
@@ -17,7 +17,7 @@ export const Footer = () => {
 
   const socialMedia = (
     <div className="flex flex-col gap-2">
-      <h2 className="mb-4">{"關注我們"}</h2>
+      <h2 className="text-xl md:text-2xl mb-4">{"關注我們"}</h2>
       <div className="w-full flex flex-row gap-2 md:gap-4">
         {SOCIAL_MEDIA.map((media, index) => (
           <Link key={`social-media-link-${index}`} href={media.url} target="_blank" rel="noopener noreferrer">
@@ -35,12 +35,12 @@ export const Footer = () => {
 
   // TODO: fix divider color issue
   return (
-    <div className="max-w-screen-2xl w-full flex flex-col lg:flex-row justify-between items-center px-24 my-8 gap-8">
+    <div className="max-w-screen-2xl w-full flex flex-col lg:flex-row justify-between items-center px-12 md:px-24 my-8 gap-8">
       <div className="w-full lg:w-1/2 flex flex-col items-center">
         {logo}
         {desc}
       </div>
-      <div className={"w-full lg:w-1/2 flex flex-col divide-y divide-gray-500 justify-center pr-8 gap-4"}>
+      <div className={"w-full lg:w-1/2 flex flex-col divide-y divide-gray-500 justify-center gap-4"}>
         <div className="flex flex-row justify-start items-start gap-4 md:gap-16">
           {about}
           {socialMedia}
