f65c7bb9183241135f1b818576b80629a8f88474
duidae <duidae@gmail.com>
2026-06-10 14:35:26 +0800

    fix: add river data

--- src/app/api/local-rivers/route.ts
+++ src/app/api/local-rivers/route.ts
@@ -4,7 +4,7 @@ import path from "path"
 
 export async function GET() {
   try {
-    const filePath = path.join(process.cwd(), "river-watcher", "data", "pcc", "rivers20191022.geojson")
+    const filePath = path.join(process.cwd(), "public", "rivers20191022.geojson")
     const content = await fs.readFile(filePath, "utf-8")
     return new NextResponse(content, {
       status: 200,
